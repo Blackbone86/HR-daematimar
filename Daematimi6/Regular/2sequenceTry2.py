4f55ed6b23f5d24db5a419a539d16e6faac303ad
Kolbeinn Grimsson <kolbeinn@outlook.com>
2020-09-03 18:44:56 +0000

    Update 2sequenceTry2.py

--- Daematimi6/Regular/2sequenceTry2.py
+++ Daematimi6/Regular/2sequenceTry2.py
@@ -6,8 +6,6 @@ num3 = 1
 num_sum = 0
 
 for i in range(n):
-    num1 = num2
-    num2 = num3
-    num3 = num_sum
+    num1, num2, num3 = num2, num3, num_sum
     num_sum = num1 + num2 + num3
     print(num_sum)
